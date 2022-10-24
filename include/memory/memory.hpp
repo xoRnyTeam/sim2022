110c62813ca202bfacb026f8299a819235810c70
dimanteo <dikolobay@gmail.com>
2022-10-24 23:51:14 +0300

    [memory] Add memory module
    Implemented basic physical memory module. For start, it just wraps an
    array of bytes. Later this module should be refactored to support MMU
    virtual to physical address translation.
    Also created header file for project common definitions.

--- include/memory/memory.hpp
+++ include/memory/memory.hpp
@@ -1,13 +1,28 @@
 #ifndef MEMORY_HPP
 #define MEMORY_HPP
 
+#include <array>
+#include <limits>
+
+#include "utils.hpp"
+
 namespace sim {
 
 /**
- * @brief
- *
+ * @brief Class representing physical memory
  */
-class Memory final {};
+class Memory final {
+  static constexpr uint32_t MEM_SIZE = std::numeric_limits<paddress_t>::max();
+  std::array<byte_t, MEM_SIZE> m_mem;
+
+public:
+  /// @brief Write \p nbytes bytes from \p src to \p addr in physical memory
+  void writeRaw(paddress_t addr, const byte_t *src, size_t nbytes);
+  /// @brief Read \p nbytes bytes from physical address \p addr to dst pointer
+  void readRaw(paddress_t addr, byte_t *dst, size_t nbytes) const;
+  /// @brief Fill memory with value
+  void memfill(paddress_t addr, byte_t val, size_t nbytes);
+};
 //
 } // namespace sim
 
