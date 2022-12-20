8dce175683098716dae16bdccaedaabca60dc34b
Dmitrryy <da.drozdov@mail.ru>
2022-12-20 17:07:26 +0300

    [Opt] Reduced the size of the 'Instruction' structure

--- include/support.hpp
+++ include/support.hpp
@@ -11,13 +11,13 @@ using paddress_t = std::uint32_t;
 using vaddress_t = std::uint32_t;
 //
 using reg_val_t = std::uint32_t;
-using reg_id_t = std::uint32_t;
+using reg_id_t = std::uint8_t;
 //
 using word_t = std::uint32_t;
 using hword_t = std::uint16_t;
 using bword_t = std::uint8_t;
 //
-using op_t = std::uint32_t;
+using op_t = std::uint8_t;
 using imm_t = std::int32_t;
 //
 constexpr reg_id_t RegNum = 32;
