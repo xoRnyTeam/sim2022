4bc581b4b27a442bcc2a26b710f33b101f552eb2
Dmitrryy <da.drozdov@mail.ru>
2022-12-20 17:02:26 +0300

    [driver][Opt] Implement caching of linear blocks

--- src/driver/driver.cpp
+++ src/driver/driver.cpp
@@ -1,4 +1,5 @@
 #include "driver/driver.hpp"
+#include "utils/compiler.hpp"
 
 namespace sim {
 
@@ -15,19 +16,41 @@ Driver::Driver(const std::string &path, const std::string &path_trace) {
 }
 
 void Driver::run() {
+  m_instCounter = 0;
+
   while (!is_terminate()) {
-    word_t cur_word = m_hart.memory.readWord(m_hart.pc);
-    auto instr = m_decoder.decode(cur_word);
+    auto &&BB = lookupBB(m_hart.pc);
+    for (Instruction &instr : BB) {
+      // save old state
+      paddress_t tmp_pc = m_hart.pc;
 
-    trace_out << "*************************************************************"
-                 "******************"
-              << std::endl;
-    trace_out << std::hex << "0x" << m_hart.pc << ": " << InstrId2str[instr.id]
-              << std::dec << " rd = " << instr.rd << ", rs1 = " << instr.rs1
-              << ", rs2 = " << instr.rs2 << ", rs3 = " << instr.rs3 << std::hex
-              << ", imm = 0x" << instr.imm << std::dec << std::endl;
+      // execute instr
+      m_executor.exec(instr, m_hart);
 
-    m_executor.exec(instr, m_hart);
+      // print trace with using old state
+      if (unlikely(trace_out.is_open())) {
+        trace_out
+            << "*************************************************************"
+               "******************"
+            << std::endl;
+        trace_out << std::hex << "0x" << tmp_pc << ": " << InstrId2str[instr.id]
+                  << std::dec << " rd = " << (int)instr.rd
+                  << ", rs1 = " << (int)instr.rs1
+                  << ", rs2 = " << (int)instr.rs2
+                  << ", rs3 = " << (int)instr.rs3 << std::hex << ", imm = 0x"
+                  << instr.imm << std::dec << std::endl;
+
+        // trace PC change
+        trace_out << "PC = 0x" << std::hex << m_hart.getPC() << '\n';
+
+        // it is assumed that the result is always rd.
+        if (instr.rd != 0) {
+          trace_out << "x" << instr.rd << " = 0x" << std::hex
+                    << m_hart.getReg(instr.rd) << '\n';
+        }
+      }
+      m_instCounter++;
+    }
     //
   }
 }
@@ -37,6 +60,44 @@ void Driver::dumpRegFile(std::ostream &outs) const {
   for (reg_id_t reg = 0; reg < RegNum; ++reg) {
     outs << "x" << reg << " : " << m_hart.getReg(reg) << "\n";
   }
+}
+
+std::vector<Instruction> Driver::lookupBB(paddress_t addr) {
+  if (!m_nativeBBChache.count(addr)) {
+    Instruction instr;
+    paddress_t cur_addr = addr;
+    std::vector<Instruction> res;
+    do {
+      word_t cur_word = m_hart.memory.readWord(cur_addr);
+      instr = m_decoder.decode(cur_word);
+      res.push_back(instr);
+      cur_addr += 4;
+    } while (!is_terminate(instr.id));
+
+    m_nativeBBChache.emplace(addr, res);
+  }
+  return m_nativeBBChache.at(addr);
+}
+
+bool Driver::is_terminate(InstrId id) {
+  switch (id) {
+  case InstrId::BEQ:
+  case InstrId::BGE:
+  case InstrId::BGEU:
+  case InstrId::BLT:
+  case InstrId::BLTU:
+  case InstrId::BNE:
+  case InstrId::EBREAK:
+  case InstrId::ECALL:
+  case InstrId::JAL:
+  case InstrId::JALR:
+  case InstrId::PAUSE:
+  case InstrId::SBREAK:
+  case InstrId::SCALL:
+    return true;
+    break;
+  }
+  return false;
 }
 
 } // namespace sim
