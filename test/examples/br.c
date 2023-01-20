fec529e6bb8cf1c56ea00d2f794c087e0c5ff29b
Dmitrryy <da.drozdov@mail.ru>
2023-01-20 12:03:34 +0300

    [jit] Tests

--- test/examples/br.c
+++ test/examples/br.c
@@ -1,6 +1,5 @@
 
-// /home/ddrozdov/tools/bin/riscv32-embecosm-ubuntu2204-gcc12.2.0/bin/riscv32-unknown-elf-gcc
-// -march=rv32i br.c -S -O0
+// /home/ddrozdov/tools/bin/riscv32-embecosm-ubuntu2204-gcc12.2.0/bin/riscv32-unknown-elf-gcc -march=rv32i 8-queens.c -e main -O1 -o 8.out
 
 int main() {
 
@@ -8,14 +7,14 @@ int main() {
   int b = 1;
 
   int fib = 0;
-  for (int i = 0; i < 10000000; ++i) {
+  for (int i = 0; i < 100000000; ++i) {
     fib = a + b;
     a = b;
     b = fib;
   }
 
   asm("ecall");
-  printf("%d", fib);
+  //printf("%d", fib);
 
-  return 0;
+  return fib;
 }
